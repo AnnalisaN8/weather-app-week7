89e753bbc724ba0b523f115bafcc312008c9e223
AnnalisaN8 <88476122+AnnalisaN8@users.noreply.github.com>
2022-03-11 12:44:15 +0100

    Showing the current temperature+wind+humidity

--- src/app.js
+++ src/app.js
@@ -1,67 +1,19 @@
-let now = new Date();
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[now.getDay()];
-let hour = now.getHours();
-if (hour < 10) {
-  hour = `0${hour}`;
-}
-let minute = now.getMinutes();
-if (minute < 10) {
-  minute = `0${minute}`;
+function displayTemp(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
-let todayDate = document.querySelector(".today-date");
-todayDate.innerHTML = `${day} ${hour}:${minute}`;
+let apiKey = "caacaca526de1415f6c61fb8c2edfa11";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New%20York&appid=${apiKey}&units=metric`;
 
-function searchLocation(position) {
-  let apiKey = "caacaca526de1415f6c61fb8c2edfa11";
-  let apiCurrentUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
-  axios.get(apiCurrentUrl).then(showTemp);
-}
-
-function getCurrentPosition(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchLocation);
-}
-
-function showTemp(response) {
-  document.querySelector("#city-name").innerHTML = response.data.name;
-  document.querySelector("#now-temp").innerHTML = `${Math.round(
-    response.data.main.temp
-  )}°C`;
-  document.querySelector("#max-temp").innerHTML = `${Math.round(
-    response.data.main.temp_max
-  )}°C`;
-  document.querySelector("#min-temp").innerHTML = `${Math.round(
-    response.data.main.temp_min
-  )}°C`;
-}
-
-function searchCity(city) {
-  let apiKey = "caacaca526de1415f6c61fb8c2edfa11";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showTemp);
-}
-
-function changeCity(event) {
-  event.preventDefault();
-  let city = document.querySelector("#city-input").value;
-
-  searchCity(city);
-}
-
-let citySearch = document.querySelector("#city-search");
-citySearch.addEventListener("submit", changeCity);
-
-let positionBtn = document.querySelector("#current-position");
-positionBtn.addEventListener("click", getCurrentPosition);
-
-searchCity("Rome");
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemp);
